410f23460a2b1d52ce87dbd64c349ec3b5de0e6f
Peggy Lu <peggy.py.lu@gmail.com>
2020-09-22 19:27:17 -0700

    test basics

---
+++ files/form_basic_ja/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,_(g,h,i,_(j,k,l,m)),n,[],o,_(h,p),q,[r],s,_(t,u,v,w,g,x,y,_(z,A,B,C,D,_(E,F,G,H),I,null,J,K,K,L,M,N,null,O,P,Q,R,S,T,U,P,V,W,_(E,F,G,X),Y,P,Z,ba,_(bb,bc,bd,be,bf,be,bg,be,bh,m,G,_(bi,bj,bk,bj,bl,bj,bm,bn)),i,_(j,k,l,m)),bo,_(),bp,_(),bq,_(br,[])),bs,_(),bt,_());}; 
+var b="url",c="form_basic_ja.html",d="generationDate",e=new Date(1600827612012.43),f="defaultAdaptiveView",g="name",h="",i="size",j="width",k=1024,l="height",m=0,n="adaptiveViews",o="sketchKeys",p="s0",q="variables",r="OnLoadVariable",s="page",t="packageId",u="1518dfe5693546dcb9e2fddbd5548131",v="type",w="Axure:Page",x="Form_basic_ja",y="style",z="baseStyle",A="627587b6038d43cca051c114ac41ad32",B="pageAlignment",C="center",D="fill",E="fillType",F="solid",G="color",H=0xFFFFFFFF,I="image",J="imageAlignment",K="near",L="imageRepeat",M="auto",N="favicon",O="sketchFactor",P="0",Q="colorStyle",R="appliedColor",S="fontName",T="Applied Font",U="borderWidth",V="borderVisibility",W="borderFill",X=0xFF797979,Y="cornerRadius",Z="cornerVisibility",ba="outerShadow",bb="on",bc=false,bd="offsetX",be=5,bf="offsetY",bg="blurRadius",bh="spread",bi="r",bj=0,bk="g",bl="b",bm="a",bn=0.349019607843137,bo="adaptiveStyles",bp="interactionMap",bq="diagram",br="objects",bs="masters",bt="objectPaths";
+return _creator();
+})());
